123821af147a7278a46abce7f37535b190d8c979
Hyun Jin, Lee <nagi5924@gmail.com>
2023-04-07 16:59:58 +0900

    패치 데이터 적용 실패

--- src/static/todoList.js
+++ src/static/todoList.js
@@ -2,112 +2,134 @@ const todoList = [
   {
     id : 0,
     content : `고영 이뻐하기`,
-    createdAt : `Wed Feb 01 2023 22:51:17 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:51:17 GMT+0900`,
+    checked : false
   },
   {
     id : 1,
     content : `고영 밥주기`,
-    createdAt : `Wed Feb 01 2023 22:51:17 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:51:17 GMT+0900`,
+    checked : false
   },
   {
     id : 2,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 3,
     content : `고영 츄르 주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 4,
     content : `고영 놀아주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 5,
     content : `고영 발톱깎기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 6,
     content : `고영 빗겨주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 8,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 9,
     content : `고영 분명히 아까 밥먹었는데도 불쌍하게 울면서 밥그릇을 떨구며 밥 달라고 시위하는 것 무시하고 제시간에 정량만 밥 주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 10,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 11,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 12,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 13,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 14,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 15,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 16,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 17,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 18,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 19,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 20,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 12,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
   {
     id : 21,
     content : `고영 궁디팡팡 해주기`,
-    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`
+    createdAt : `Wed Feb 01 2023 22:53:57 GMT+0900`,
+    checked : false
   },
 
 ]
